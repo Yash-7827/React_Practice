7a485ba1333429ed0ef5fe74e8682fc6a9e828f4
Yash-7827 <55312760+Yash-7827@users.noreply.github.com>
2022-07-01 19:24:08 +0530

    Enter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Layout/Header'
 import React from 'react';
+import Meals from './Components/Meals/Meals'
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
+      <Meals/>
     </React.Fragment>
   );
 }
---
+++ src/Components/Meals/MealsItem/MealItem.js
@@ -0,0 +1,11 @@
+import classes from './MealItem.module.css'
+
+const MealItem = (props) => {
+    return(
+        <li className={classes.meal}>
+
+        </li>
+    );
+}
+
+export default MealItem;
